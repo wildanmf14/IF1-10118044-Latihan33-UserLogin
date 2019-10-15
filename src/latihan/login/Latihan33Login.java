3b7aa92092bc8678cd0c96295053f03e74b8a650
Wildhevire <wildhevire404@gmail.com>
2019-10-15 21:20:58 +0000

    finished, revisi hak akses di main class

--- src/latihan/login/Latihan33Login.java
+++ src/latihan/login/Latihan33Login.java
@@ -1,14 +1,15 @@
 package latihan.login;
 import java.util.Scanner;
 public class Latihan33Login {
-
+	private static String usName;
+	private static String passWord;
 	public static void main(String[] args) {
 		Scanner reader = new Scanner(System.in);
 		User user = new User();
 		System.out.print("Masukkan Username : ");
-		String usName = reader.next();
+		usName = reader.next();
 		System.out.print("Masukkan Password : ");
-		String passWord = reader.next();
+		passWord = reader.next();
 		user.pengecekanLogin(usName, passWord);
 		
 		reader.close();
